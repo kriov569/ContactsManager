ed8eb6275c8687d1b1ab3124f16d5213f1570d02
kriov569 <nikita.veshnyakov2014@gmail.com>
2024-09-22 00:00:02 +0300

    Добавил валидацию

--- src/main/java/com/example/demo/Contact.java
+++ src/main/java/com/example/demo/Contact.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Contact {
@@ -11,9 +15,20 @@ public class Contact {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
+
+    @NotBlank(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 50, message = "Имя должно содержать от 2 до 50 символов")
     private String firstName;
+
+    @NotBlank(message = "Фамилия не должна быть пустым")
+    @Size(min = 2, max = 50, message = "Фамилия должна содержать от 2 до 50 символов")
     private String lastName;
+
+    @NotNull(message = "Телефон не должен быть пустым")
     private String phoneNumber;
+
+    @NotBlank(message = "Email не должен быть пустым")
+    @Email(message = "Неверный формат email")
     private String email;
 
     public Long getId() {
--- src/main/java/com/example/demo/ContactController.java
+++ src/main/java/com/example/demo/ContactController.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/contacts")
+@Validated
 public class ContactController {
     @Autowired
     private ContactService service;
@@ -21,13 +24,13 @@ public class ContactController {
     }
 
     @PostMapping
-    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+    public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         Contact savedContact = service.saveContact(contact);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedContact);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contact) {
+    public ResponseEntity<Contact> updateContact(@PathVariable Long id, @Valid @RequestBody Contact contact) {
         try {
             Contact updatedContact = service.updateContact(id, contact);
             return ResponseEntity.ok(updatedContact);
